28933169f95f8cfc5b559297a7a10097adafc1d9
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-06 13:18:32 +0000

    Add files via upload

--- asterisk_install.sh
+++ asterisk_install.sh
@@ -1,41 +1,48 @@
 #!/bin/bash
 
-# INSTALL ASTERISK
-read -p "Do use want to install Asterisk (y/n)" INSTALL_OPTION
-if [ "$INSTALL_OPTION" = "y" ];
-then
-echo "Install asterisk..."
-sudo apt-get install xmlstarlet libopus-dev libopusfile-dev
-wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-20-current.tar.gz
-tar -xvf asterisk-20-current.tar.gz
+echo $USER
 
-echo "here is your unzip file:"
-ls
-read -p "Enter your unzip Asterisk folder:" ASTERISK_FOLDER
-cd /home/pi/$ASTERISK_FOLDER
-sudo contrib/scripts/install_prereq install
-sudo ./configure --with-pjproject-bundled
-sudo make menuselect
-sudo make
-sudo make install
+# INSTALL ASTERISK
+read -p "Do you want to download and install Asterisk (y/n)" DOWNLOAD_OPTION
+if [ "$DOWNLOAD_OPTION" = "y" ];
+then
+sudo apt-get install xmlstarlet libopus-dev libopusfile-dev
+
+# Check if file existed
+if [ ! -f "/home/pi/asterisk-20-current.tar.gz" ];
+then
+echo "Download asterisk-20-current.tar.gz"
+wget -P /home/pi http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-20-current.tar.gz
+fi
+
+# Unzip file
+sudo rm -rf /home/pi/asterisk-20-current
+mkdir /home/pi/asterisk-20-current
+tar -xvf /home/pi/asterisk-20-current.tar.gz -C /home/pi/asterisk-20-current --strip-components=1
+
+echo "Install asterisk..."
+cd /home/pi/asterisk-20-current && sudo contrib/scripts/install_prereq install
+cd /home/pi/asterisk-20-current && sudo ./configure --with-pjproject-bundled
+cd /home/pi/asterisk-20-current && sudo make menuselect
+cd /home/pi/asterisk-20-current && sudo make
+cd /home/pi/asterisk-20-current && sudo make install
+echo "Configure Asterisk..."
+cd /home/pi/asterisk-20-current && sudo make samples
+cd /home/pi/asterisk-20-current && sudo make config
 echo "Install Asterisk successfully"
 else
 echo "Not install Asterisk. Contninue..."
+echo "Configure Asterisk..."
+cd /home/pi/asterisk-20-current && sudo make samples
+cd /home/pi/asterisk-20-current && sudo make config
 fi
 
-# ------------------------------------------
-# CONFIGURE ASTERISK
-echo "Configure Asterisk..."
-sudo make samples
-sudo make config
-
 # Browser Phone
-cd ~
 read -p "Do you want to clone Browser-Phone.git (y/n)" BROWSER_PHONE_OPTION
 if [ "$BROWSER_PHONE_OPTION" = "y" ];
 then
 echo "Clone Browser-Phone.git..."
-git clone https://github.com/InnovateAsterisk/Browser-Phone.git
+cd ~ && git clone https://github.com/InnovateAsterisk/Browser-Phone.git
 else
 echo "Continue..."
 fi
@@ -45,13 +52,28 @@ sudo cp /home/pi/Browser-Phone/config/* /etc/asterisk/
 sudo rm /var/lib/asterisk/static-http/*
 sudo cp /home/pi/Browser-Phone/Phone/* /var/lib/asterisk/static-http/
 sudo chmod 744 /var/lib/asterisk/static-http/*
-sudo nano /etc/asterisk/http.conf
+
+sudo tee /etc/asterisk/http.conf > /dev/null <<EOT
+[general]
+enabled=no ; HTTP
+tlsenable=yes ; HTTPS
+tlsbindaddr=0.0.0.0:443
+tlscertfile=/home/pi/certs/raspberrypi.crt
+tlsprivatekey=/home/pi/certs/raspberrypi.key
+enablestatic=yes
+sessionlimit=1000
+redirect=/ /static/index.html
+EOT
+
 sudo cp /home/pi/Browser-Phone/modules/ast-16/codec_opus_arm.so /usr/lib/asterisk/modules
 echo "Copy AST_BUILDOPT_SUM:"
-cat /home/pi/asterisk-20.5.0/include/asterisk/buildopts.h
+cat /home/pi/asterisk-20-current/include/asterisk/buildopts.h
 read -p "Input AST_BUILDOPT_SUM:" AST_BUILDOPT_SUM
-sudo sed -i 's/1fb7f5c06d7a2052e38d021b3d8ca151/$AST_BUILDOPT_SUM/g' /usr/lib/asterisk/modules/codec_opus_arm.so
+sudo sed -i "s/1fb7f5c06d7a2052e38d021b3d8ca151/${AST_BUILDOPT_SUM}/g" /usr/lib/asterisk/modules/codec_opus_arm.so
 
+
+# START ASTERISK SERVICE
 echo "Start Asterisk service..."
 sudo service asterisk start
+sudo service asterisk restart
 echo "Done"
--- samba_setup.sh
+++ samba_setup.sh
@@ -7,9 +7,27 @@ sudo systemctl enable ssh --now
 echo "Install Samba"
 sudo apt-get update
 sudo apt-get install samba ntp git
-read -p "Add Samba username:" USERNAME
-sudo smbpasswd -a $USERNAME
+sudo smbpasswd -a $USER
 
 # edit smb.conf
-sudo nano /etc/samba/smb.conf
+if grep -Fxq "[InnovateAsterisk]" /etc/samba/smb.conf
+then
+echo "[InnovateAsterisk] is already added"
+else
+echo "adding [InnovateAsterisk]..."
+sudo tee -a /etc/samba/smb.conf > /dev/null <<EOT
+[InnovateAsterisk]
+path = /
+browseable = yes
+writeable = yes
+read only = no
+create mask = 0755
+directory mask = 0755
+guest ok = no
+security = user
+write list = $USER
+force user = root
+EOT
+echo "[InnovateAsterisk] is added"
+fi
 sudo service smbd restart
