f0575ee453e24888e1ed9d748b7ded4eb38ce5f8
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-06 20:53:32 +0700

    Update asterisk_install.sh

--- asterisk_install.sh
+++ asterisk_install.sh
@@ -9,32 +9,32 @@ then
 sudo apt-get install xmlstarlet libopus-dev libopusfile-dev
 
 # Check if file existed
-if [ ! -f "/home/pi/asterisk-20-current.tar.gz" ];
+if [ ! -f "/home/${USER}/asterisk-20-current.tar.gz" ];
 then
 echo "Download asterisk-20-current.tar.gz"
-wget -P /home/pi http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-20-current.tar.gz
+wget -P /home/$USER http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-20-current.tar.gz
 fi
 
 # Unzip file
-sudo rm -rf /home/pi/asterisk-20-current
-mkdir /home/pi/asterisk-20-current
-tar -xvf /home/pi/asterisk-20-current.tar.gz -C /home/pi/asterisk-20-current --strip-components=1
+sudo rm -rf /home/$USER/asterisk-20-current
+mkdir /home/$USER/asterisk-20-current
+tar -xvf /home/$USER/asterisk-20-current.tar.gz -C /home/$USER/asterisk-20-current --strip-components=1
 
 echo "Install asterisk..."
-cd /home/pi/asterisk-20-current && sudo contrib/scripts/install_prereq install
-cd /home/pi/asterisk-20-current && sudo ./configure --with-pjproject-bundled
-cd /home/pi/asterisk-20-current && sudo make menuselect
-cd /home/pi/asterisk-20-current && sudo make
-cd /home/pi/asterisk-20-current && sudo make install
+cd /home/$USER/asterisk-20-current && sudo contrib/scripts/install_prereq install
+cd /home/$USER/asterisk-20-current && sudo ./configure --with-pjproject-bundled
+cd /home/$USER/asterisk-20-current && sudo make menuselect
+cd /home/$USER/asterisk-20-current && sudo make
+cd /home/$USER/asterisk-20-current && sudo make install
 echo "Configure Asterisk..."
-cd /home/pi/asterisk-20-current && sudo make samples
-cd /home/pi/asterisk-20-current && sudo make config
+cd /home/$USER/asterisk-20-current && sudo make samples
+cd /home/$USER/asterisk-20-current && sudo make config
 echo "Install Asterisk successfully"
 else
 echo "Not install Asterisk. Contninue..."
 echo "Configure Asterisk..."
-cd /home/pi/asterisk-20-current && sudo make samples
-cd /home/pi/asterisk-20-current && sudo make config
+cd /home/$USER/asterisk-20-current && sudo make samples
+cd /home/$USER/asterisk-20-current && sudo make config
 fi
 
 # Browser Phone
@@ -48,9 +48,9 @@ echo "Continue..."
 fi
 
 echo "Config Asterisk with Browser-Phone..."
-sudo cp /home/pi/Browser-Phone/config/* /etc/asterisk/
+sudo cp /home/$USER/Browser-Phone/config/* /etc/asterisk/
 sudo rm /var/lib/asterisk/static-http/*
-sudo cp /home/pi/Browser-Phone/Phone/* /var/lib/asterisk/static-http/
+sudo cp /home/$USER/Browser-Phone/Phone/* /var/lib/asterisk/static-http/
 sudo chmod 744 /var/lib/asterisk/static-http/*
 
 sudo tee /etc/asterisk/http.conf > /dev/null <<EOT
@@ -58,16 +58,16 @@ sudo tee /etc/asterisk/http.conf > /dev/null <<EOT
 enabled=no ; HTTP
 tlsenable=yes ; HTTPS
 tlsbindaddr=0.0.0.0:443
-tlscertfile=/home/pi/certs/raspberrypi.crt
-tlsprivatekey=/home/pi/certs/raspberrypi.key
+tlscertfile=/home/$USER/certs/raspberrypi.crt
+tlsprivatekey=/home/$USER/certs/raspberrypi.key
 enablestatic=yes
 sessionlimit=1000
 redirect=/ /static/index.html
 EOT
 
-sudo cp /home/pi/Browser-Phone/modules/ast-16/codec_opus_arm.so /usr/lib/asterisk/modules
+sudo cp /home/$USER/Browser-Phone/modules/ast-16/codec_opus_arm.so /usr/lib/asterisk/modules
 echo "Copy AST_BUILDOPT_SUM:"
-cat /home/pi/asterisk-20-current/include/asterisk/buildopts.h
+cat /home/$USER/asterisk-20-current/include/asterisk/buildopts.h
 read -p "Input AST_BUILDOPT_SUM:" AST_BUILDOPT_SUM
 sudo sed -i "s/1fb7f5c06d7a2052e38d021b3d8ca151/${AST_BUILDOPT_SUM}/g" /usr/lib/asterisk/modules/codec_opus_arm.so
 
