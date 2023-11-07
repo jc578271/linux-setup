b88011ed16be11191692cbdef750ff019cfc34dd
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-07 21:31:24 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,4 +2,5 @@
 set -e
 
 read -p "Enter your backup name: " backup_name
-sudo tar  -czf /$backup_name.tar.gz --exclude "/var/swap" /usr /etc /lib /var
+sudo rm /$backup_name.tar.gz
+sudo tar  -czf /$backup_name.tar.gz --exclude="/var/swap" /usr /etc /lib /var
