3a5ec68ec9a661abad957d37ede9bd8dbf800d9f
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-07 19:32:09 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -5,20 +5,25 @@ BACKUPTMP_PATH="backup_temp_$(date +%s)"
 sudo ls /
 read -p "Input your backup path (backup): " backup_path
 
-#Unzip backup folder
-sudo tar -xf /$backup_path.tar.gz
+#Unzip backup folder to backup folder
+sudo tar -xf /$backup_path.tar.gz $backup_path/
 
 mkdir $BACKUPTMP_PATH
 
 #Find and cat to txt
-sudo find $backup_path -type f | awk '{print substr($NF, index($NF, "/")+1)}' > $BACKUPTMP_PATH/backup_list.txt
-sudo find /usr /etc /lib /var -type f | awk '{print substr($NF, index($NF, "/")+1)}' > $BACKUPTMP_PATH/current_list.txt
+sudo find $backup_path -type f | awk '{print substr($NF, index($NF, "/")+1)}' >>
+sudo find /usr /etc /lib /var -type f | awk '{print substr($NF, index($NF, "/")>
 
-awk 'FNR==NR {a[$0];next}!($0 in a) {print $0}' $BACKUPTMP_PATH/backup_list.txt $BACKUPTMP_PATH/current_list.txt | awk '{print "sudo rm -rf " $0}' > $BACKUPTMP_PATH/result.sh
+# Remove the external packages
+awk 'FNR==NR {a[$0];next}!($0 in a) {print $0}' $BACKUPTMP_PATH/backup_list.txt>
 
+echo "Remove external packages..."
 bash $BACKUPTMP_PATH/result.sh
-echo "Run bash..."
-cat  $BACKUPTMP_PATH/result.sh
 
 rm -rf $BACKUPTMP_PATH
 sudo rm -rf $backup_path
+
+# Reset system
+echo "Reset system..."
+cd / && sudo tar --recursive-unlink -xf $backup_path.tar.gz
+
