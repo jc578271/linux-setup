0adee53007ec00e02a0590d110ebf10e46c19de5
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-07 19:23:36 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 read -p "Enter your backup name: " backup_name
-sudo mkdir /$backup_name
-sudo rsync -aAX /usr /etc /lib /var /$backup_name
-sudo tar -czf /$backup_name.tar.gz /$backup_name
-sudo rm -rf /$backup_name
+sudo tar  -czf /$backup_name.tar.gz /usr /etc /lib /var
