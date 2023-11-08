cab1ce6960b605d83fe643b8d5ca4ddbac3798b2
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-08 20:30:44 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@ read -p "Enter your backup name: " backup_name
 
 sudo rm /$backup_name.tar.gz
 
-sudo tar --ignore-failed-read --exclude="/var/swap" --checkpoint=100 --totals --checkpoint-action="exec='p=\$(echo "\$TAR_CHECKPOINT/$blocks" | bc -l);printf \"%.4f%%\r\" \$p'" -czf /$backup_name.tar.gz /usr /etc /lib /var/*
+sudo tar --ignore-failed-read --exclude="/var/swap" --checkpoint=100 --totals --checkpoint-action="exec='p=\$(echo "$TAR_CHECKPOINT" -czf /$backup_name.tar.gz /usr /etc /lib /var/*
