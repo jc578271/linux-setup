654b43bc4c85826c49a25d482809368838d357f3
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-08 12:01:02 +0700

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -4,7 +4,20 @@ sudo mkdir /home/$USER/ca /home/$USER/certs /home/$USER/csr
 sudo openssl genrsa -des3 -out /home/$USER/ca/InnovateAsterisk-Root-CA.key 4096
 sudo openssl req -x509 -new -nodes -key /home/$USER/ca/InnovateAsterisk-Root-CA.key -sha256 -days 3650 -out /home/$USER/ca/InnovateAsterisk-Root-CA.crt
 sudo openssl req -new -sha256 -nodes -out /home/$USER/csr/raspberrypi.csr -newkey rsa:2048 -keyout /home/$USER/certs/raspberrypi.key
-sudo nano /home/$USER/csr/openssl-v3.cnf
+
+read -p "Enter your DNS (raspberry.local): " DNS
+
+sudo tee -a /etc/samba/smb.conf > /dev/null <<EOT
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = $DNS
+EOT
+# sudo nano /home/$USER/csr/openssl-v3.cnf
+
 sudo openssl x509 -req -in /home/$USER/csr/raspberrypi.csr -CA /home/$USER/ca/InnovateAsterisk-Root-CA.crt -CAkey /home/$USER/ca/InnovateAsterisk-Root-CA.key -CAcreateserial -out /home/$USER/certs/raspberrypi.crt -days 365 -sha256 -extfile /home/$USER/csr/openssl-v3.cnf
 sudo cat /home/$USER/certs/raspberrypi.crt /home/$USER/certs/raspberrypi.key > /home/$USER/certs/raspberrypi.pem
 sudo chmod a+r /home/$USER/certs/raspberrypi.key
