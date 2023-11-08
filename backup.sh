f6469bb7dcab93b8316dab9faae51a3b76de89f5
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-08 17:40:48 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@ read -p "Enter your backup name: " backup_name
 
 sudo rm /$backup_name.tar.gz
 
-sudo pv /$backup_name.tar.gz | tar --ignore-failed-read --exclude="/var/swap" -czf /usr /etc /lib /var/*
+sudo pv /$backup_name.tar.gz | tar --ignore-failed-read --exclude="/var/swap" -czf /$backup_name.tar.gz /usr /etc /lib /var/*
