7f47dbf662a1ec5e6ec2afabe40dddf018536364
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-08 14:08:41 +0700

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -11,9 +11,9 @@ read -p "Common Name : " COMMON
 read -p "Email Address: " EMAIL
 
 sudo mkdir /home/$USER/ca /home/$USER/certs /home/$USER/csr
-sudo openssl genrsa -des3 -out /home/$USER/ca/InnovateAsterisk-Root-CA.key 4096 -pass pass:$PASSWD
-sudo openssl req -x509 -new -nodes -key /home/$USER/ca/InnovateAsterisk-Root-CA.key -sha256 -days 3650 -out /home/$USER/ca/InnovateAsterisk-Root-CA.crt -subj "/C=${COUNTRY}/ST=${PROVINCE}/L=${CITY}/O=${COMPANY}/OU=${UNIT}/CN=${COMMON}/emailAddress=${EMAIL}" -pass pass:$PASSWD
-sudo openssl req -new -sha256 -nodes -out /home/$USER/csr/raspberrypi.csr -newkey rsa:2048 -keyout /home/$USER/certs/raspberrypi.key -subj "/C=${COUNTRY}/ST=${PROVINCE}/L=${CITY}/O=${COMPANY}/OU=${UNIT}/CN=${COMMON}/emailAddress=${EMAIL}" -pass pass:$PASSWD
+sudo openssl genrsa -des3 -out /home/$USER/ca/InnovateAsterisk-Root-CA.key 4096
+sudo openssl req -x509 -new -nodes -key /home/$USER/ca/InnovateAsterisk-Root-CA.key -sha256 -days 3650 -out /home/$USER/ca/InnovateAsterisk-Root-CA.crt -subj "/C=${COUNTRY}/ST=${PROVINCE}/L=${CITY}/O=${COMPANY}/OU=${UNIT}/CN=${COMMON}/emailAddress=${EMAIL}"
+sudo openssl req -new -sha256 -nodes -out /home/$USER/csr/raspberrypi.csr -newkey rsa:2048 -keyout /home/$USER/certs/raspberrypi.key -subj "/C=${COUNTRY}/ST=${PROVINCE}/L=${CITY}/O=${COMPANY}/OU=${UNIT}/CN=${COMMON}/emailAddress=${EMAIL}"
 
 read -p "Enter your DNS (raspberry.local): " DNS
 
@@ -28,7 +28,7 @@ DNS.1 = $DNS
 EOT
 # sudo nano /home/$USER/csr/openssl-v3.cnf
 
-sudo openssl x509 -req -in /home/$USER/csr/raspberrypi.csr -CA /home/$USER/ca/InnovateAsterisk-Root-CA.crt -CAkey /home/$USER/ca/InnovateAsterisk-Root-CA.key -CAcreateserial -out /home/$USER/certs/raspberrypi.crt -days 365 -sha256 -extfile /home/$USER/csr/openssl-v3.cnf -pass pass:$PASSWD
+sudo openssl x509 -req -in /home/$USER/csr/raspberrypi.csr -CA /home/$USER/ca/InnovateAsterisk-Root-CA.crt -CAkey /home/$USER/ca/InnovateAsterisk-Root-CA.key -CAcreateserial -out /home/$USER/certs/raspberrypi.crt -days 365 -sha256 -extfile /home/$USER/csr/openssl-v3.cnf
 sudo cat /home/$USER/certs/raspberrypi.crt /home/$USER/certs/raspberrypi.key > /home/$USER/certs/raspberrypi.pem
 sudo chmod a+r /home/$USER/certs/raspberrypi.key
 echo "Done!"
