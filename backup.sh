25bd6372a38dbd742faf9144b4a4ec7b75bd097a
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-08 17:19:30 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,4 +3,8 @@
 read -p "Enter your backup name: " backup_name
 sudo rm /$backup_name.tar.gz
 
-sudo tar --ignore-failed-read --exclude="/var/swap" -czf /$backup_name.tar.gz /usr /etc /lib /var/*
+sudo tar --ignore-failed-read --exclude="/var/swap" -czf /$backup_name.tar.gz /usr /etc /lib /var/* | 
+while read line; do
+    x=$((x+1))
+    echo -en "$x extracted\r"
+done
