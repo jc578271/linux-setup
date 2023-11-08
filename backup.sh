f1bd25128a2f5355f588cf0c7ffcef40490e1179
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-08 17:39:27 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 read -p "Enter your backup name: " backup_name
+
 sudo rm /$backup_name.tar.gz
 
-sudo tar --ignore-failed-read --exclude="/var/swap" --checkpoint=1000 --checkpoint-action=dot -czf /$backup_name.tar.gz /usr /etc /lib /var/* |
-while read line; do
-    echo $line
-done
+sudo pv /$backup_name.tar.gz | tar --ignore-failed-read --exclude="/var/swap" -czf /usr /etc /lib /var/*
