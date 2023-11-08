855d75d02ff5796ddf97b55cf8ec3062b6ee2d96
jc578271 <57795010+jc578271@users.noreply.github.com>
2023-11-08 23:56:44 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,6 +2,6 @@
 
 read -p "Enter your backup name: " backup_name
 
-sudo rm /$backup_name.tar.gz
+sudo rm -rf /$backup_name.tar.gz
 
 sudo tar --ignore-failed-read --exclude={"/var/swap","/var/lock","/var/run","/var/tmp"} -czvf /$backup_name.tar.gz /usr /etc /lib /var
