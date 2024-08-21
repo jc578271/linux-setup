a1e11549c4935df6aff6871f42cf1bf57cc6d379
jc578271 <57795010+jc578271@users.noreply.github.com>
2024-08-21 20:05:32 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@ read -p "Enter your backup name: " backup_name
 
 sudo rm -rf /$backup_name.tar.gz
 
-sudo tar --ignore-failed-read -czvf /$backup_name.tar.gz /usr /etc /lib /var
+sudo tar --ignore-failed-read -czvf /$backup_name.tar.gz /usr /etc /lib /var /home
