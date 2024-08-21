5522652b121d70f07d1dccde6bff47670b37ebba
jc578271 <57795010+jc578271@users.noreply.github.com>
2024-08-21 20:42:17 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -19,8 +19,8 @@ sudo tar --ignore-command-error -xf /$backup_path.tar.gz -C $BACKUP_TEMP
 
 #Find and cat to txt
 mkdir $LOG_TEMP
-sudo find $BACKUP_TEMP -type f | awk '{print substr($NF, index($NF, "/")+1)}' > $LOG_TEMP/backup_list.txt
-sudo find /usr /etc /lib /var /home -type f | awk '{print substr($NF, index($NF, "/")+1)}' > $LOG_TEMP/current_list.txt
+sudo find $BACKUP_TEMP -type f | awk '{print substr($0, index($0, "/")+1)}' > $LOG_TEMP/backup_list.txt
+sudo find /usr /etc /lib /var /home -type f | awk '{print substr($0, index($0, "/")+1)}' > $LOG_TEMP/current_list.txt
 
 # Remove the external packages
 awk 'FNR==NR {a[$0];next}!($0 in a) {print $0}' $LOG_TEMP/backup_list.txt $LOG_TEMP/current_list.txt | awk '{print "sudo rm -rf " $0}' > $LOG_TEMP/result.sh
